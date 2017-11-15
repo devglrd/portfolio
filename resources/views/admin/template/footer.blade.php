2c5eaafc210ab99e49072cfb7db733bad9e71572
Remi Guillard <glrd.remi@gmail.com>
2017-11-15 14:39:18 +0100

    analitycs

--- resources/views/admin/template/footer.blade.php
+++ resources/views/admin/template/footer.blade.php
@@ -4,17 +4,26 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+{{--<script>--}}
+    {{--// Initialize Firebase--}}
+    {{--var config = {--}}
+        {{--apiKey: "AIzaSyAphQcFnyxLcisF5l55UAD3C5dnMAIY4PA",--}}
+        {{--authDomain: "gifs-183fa.firebaseapp.com",--}}
+        {{--databaseURL: "https://gifs-183fa.firebaseio.com",--}}
+        {{--projectId: "gifs-183fa",--}}
+        {{--storageBucket: "gifs-183fa.appspot.com",--}}
+        {{--messagingSenderId: "409440471110"--}}
+    {{--};--}}
+    {{--firebase.initializeApp(config);--}}
+{{--</script>--}}
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109732774-1"></script>
 <script>
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyAphQcFnyxLcisF5l55UAD3C5dnMAIY4PA",
-        authDomain: "gifs-183fa.firebaseapp.com",
-        databaseURL: "https://gifs-183fa.firebaseio.com",
-        projectId: "gifs-183fa",
-        storageBucket: "gifs-183fa.appspot.com",
-        messagingSenderId: "409440471110"
-    };
-    firebase.initializeApp(config);
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-109732774-1');
 </script>
 </body>
 </html>
