b0fe3b650c3acb9d163157927f15773dc18409b0
Remi Guillard <glrd.remi@gmail.com>
2017-11-15 10:07:48 +0100

    add boostrap 4

--- resources/views/admin/entities/gif.blade.php
+++ resources/views/admin/entities/gif.blade.php
@@ -4,37 +4,39 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard
+            <div class="col-md-8 offset-2">
+                <div class="card ">
+                    <div class="card-header">Dashboard
                         <a href="">
                             <button class="btn btn-primary">Reload</button>
                         </a>
                     </div>
 
-                    <div class="panel-body">
+                    <div class="card-body">
                         <img src="{{ $unique }}" alt="">
                     </div>
-                    <div class="panel-footer">
+                    <div class="card-footer">
                         Partagé le gif : <a href="{{ $unique }}" target="_blank"><span>{{ $unique }}</span></a>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="row">
-            <h3 class="text-center jumbotron">Gifs déjà généré</h3>
+        <div class="row mt-4">
+            <div class="col">
+                <h3 class="text-center jumbotron d-flex justify-content-center">Gifs déjà généré</h3>
+            </div>
         </div>
 
         <div class="row">
             @if($allGif->count() > 1 )
             @foreach($allGif as $gif)
-            <div class="col-md-5">
-                <div class="panel panel-default">
-                    <div class="panel-body">
+            <div class="col">
+                <div class="card ">
+                    <div class="card-body">
                         <img src="{{ $gif->img_url }}" alt="">
                     </div>
-                    <div class="panel-footer">
+                    <div class="card-footer">
                         Partagé le gif : <a href="{{ $gif->img_url }}" target="_blank"><span>{{ $gif->img_url }}</span></a>
                     </div>
                 </div>
--- resources/views/admin/template/navbar.blade.php
+++ resources/views/admin/template/navbar.blade.php
@@ -1,57 +1,17 @@
 <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-coontent-between mb-3">
         <div class="container">
-            <div class="navbar-header">
+            <a class="navbar-brand" href="#">{{ config('app.name', 'Rémi') }}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Rémi') }}
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @guest
-                    <li><a href="#">RandomGiphy</a></li>
-                    {{--<li><a href="{{ route('login') }}">Login</a></li>--}}
-                    {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                        @endguest
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">RandomGiphy <span class="sr-only">(current)</span></a>
+                    </li>
                 </ul>
             </div>
         </div>
-</nav>
+    </nav>
